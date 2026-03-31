df65b891101f9b684401b694caf931aaa8edcb1b
guilhermeanunes <guilhermeaugustonunes.2005@gmail.com>
2026-03-31 20:29:55 -0300

    Organizando pastas e upando restante dos arquivos
    Este commit cria mais alguns arquivos referente à atividade prática 01 parte 2, além disso organiza melhor as partes.

---
+++ atividade_pratica_parte02/atv01/main.py
@@ -0,0 +1,12 @@
+from pratica02_01 import *
+
+def main():
+    carro1 = Carro("Chevrolet", "Onix", 2020, 4)
+    carro1.exibir_info()
+
+    print("\n")
+
+    moto1 = Moto("Honda", "CB", 2013, 300)
+    moto1.exibir_info()
+
+main()
---
+++ atividade_pratica_parte02/atv01/pratica02_01.py
@@ -0,0 +1,38 @@
+class Veiculo:
+    def __init__(self, marca: str, modelo: str, ano_fabricacao: int):
+        if not marca.strip():
+            raise TypeError ("A marca não pode ser vazia.")
+        self.marca = marca
+
+        if not modelo.strip():
+            raise TypeError ("O modelo não pode ser vazio.")
+        self.modelo = modelo
+
+        if not isinstance(ano_fabricacao, int):
+            raise TypeError ("Digite o ano corretamente")
+        self.ano_fabricacao = ano_fabricacao
+
+    def exibir_info(self):
+        print(f"Veículo: {self.marca} {self.modelo} — Ano: {self.ano_fabricacao}")
+    
+class Carro(Veiculo):
+    def __init__ (self, marca: str, modelo: str, ano_fabricacao: int, numero_portas: int):
+        super().__init__(marca, modelo, ano_fabricacao)
+        if numero_portas not in [2, 4]:
+            raise ValueError ("A quantidade de portas deve ser 2 ou 4")
+        self.numero_portas = numero_portas
+    
+    def exibir_info(self):
+        super().exibir_info()
+        print(f"Número de portas: {self.numero_portas}")
+    
+class Moto(Veiculo):
+    def __init__(self, marca: str, modelo: str, ano_fabricacao: int, cilindrada: int):
+        super().__init__(marca, modelo, ano_fabricacao)
+        if cilindrada <= 0:
+            raise ValueError ("A cilindrada deve ser maior que 0")
+        self.cilindrada = cilindrada
+    
+    def exibir_info(self):
+        super().exibir_info()
+        print(f"Cilindrada: {self.cilindrada}")
